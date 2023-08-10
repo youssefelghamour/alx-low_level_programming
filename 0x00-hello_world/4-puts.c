de33c08ededdd2895b77abf8b99eccb77bcfd956
youssef <youssefelghamour@gmail.com>
2023-08-10 05:21:34 -0700

    add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints a sentence using puts function
+ *
+ * Return: Always  0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
