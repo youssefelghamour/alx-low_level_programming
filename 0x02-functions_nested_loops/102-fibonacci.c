3b64bba9de7b45b2ae65df5ad94b9d8f70777ce3
youssef <youssefelghamour@gmail.com>
2023-08-15 15:21:18 -0700

    change 102-fibonacci.c - arrays not supported

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,24 +9,18 @@
 int main(void)
 {
 	int i;
-	long int j, k;
-	long int numb[i];
+	long int a, b, c;
 
-	numb[0] = 0;
-	numb[1] = 1;
-
-	for (i = 2; i < 48; i++)
+	a = 1;
+	b = 2;
+	printf("%ld, %ld", a, b);
+	for (i = 0; i < 48; i++)
 	{
-		k = numb[i - 1];
-		j = numb[i - 2];
-		numb[i] = j + k;
-		if (numb[i] != 1)
-		{
-			printf(", ");
-		}
-		printf("%ld", numb[i]);
+		c = a + b;
+		printf(", %ld", c);
+		a = b;
+		b = c;
 	}
-
 	printf("\n");
 	return (0);
 }
