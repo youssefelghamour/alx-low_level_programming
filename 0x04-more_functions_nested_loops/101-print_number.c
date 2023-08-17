a7d788e4f78c4f06deec6de2672e06d839862c46
youssef <youssefelghamour@gmail.com>
2023-08-17 08:46:53 -0700

    add 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ *
+ * @n: input integer.
+ *
+ * Return: no return.
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+		_putchar('-');
+	}
+	if (n <= 9)
+	{
+		_putchar(n + '0');
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n > 99 && n <= 999)
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n / 100) / 10) + '0');
+		_putchar(((n % 100) % 10) + '0');
+	}
+	else if (n > 999 && n <= 9999)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n / 1000) / 100) + '0');
+		_putchar((((n / 1000) / 100) / 10) + '0');
+		_putchar((((n % 1000) % 100) % 10) + '0');
+	}
+}
