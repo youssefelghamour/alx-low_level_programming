c2d6a59879d5dbab437b1ec2bd28bc1afb09a28c
youssef <youssefelghamour@gmail.com>
2023-08-17 09:09:51 -0700

    using recursive for 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -17,32 +17,7 @@ void print_number(int n)
 	}
 	if (n <= 9)
 	{
-		_putchar(n + '0');
+		print_number(n / 10);
 	}
-	else if (n > 9 && n <= 99)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n > 99 && n <= 999)
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 100) / 10) + '0');
-		_putchar(((n % 100) % 10) + '0');
-	}
-	else if (n > 999 && n <= 9999)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 1000) / 100) + '0');
-		_putchar((((n / 1000) / 100) / 10) + '0');
-		_putchar((((n % 1000) % 100) % 10) + '0');
-	}
-	else if (n <= 99999)
-	{
-		_putchar((n / 10000) + '0');
-		_putchar(((n / 1000) % 10) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
+	_putchar(n % 10 + 48);
 }
