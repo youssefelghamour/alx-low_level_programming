80169ebf66db3b9a4dbebe58293fc347567d28a8
youssef <youssefelghamour@gmail.com>
2023-08-17 08:25:35 -0700

    add 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio>
+
+/**
+ * main - prints largest prime factor.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int i;
+	long int n = 612852475143;
+
+	for (i = 2; i <= n; i++)
+	{
+		while ((n % i) == 0)
+		{
+			n = n / i;
+		}
+	}
+
+	printf("%ld\n", i - 1);
+	return (0);
+}
