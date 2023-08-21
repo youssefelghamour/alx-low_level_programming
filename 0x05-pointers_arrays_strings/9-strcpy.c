dca48391c00cd114b213309dc7af17ece28dda0f
youssef <youssefelghamour@gmail.com>
2023-08-21 09:59:13 -0700

    add 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte, to the
+ * buffer pointed to by dest.
+ * @dest: destination.
+ * @src: source.
+ * Return: the pointer to dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0, i;
+
+	while (src[l])
+		l++;
+
+	for (i = 0; i < l; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
