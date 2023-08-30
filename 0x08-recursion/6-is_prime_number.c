8fc18fba7e8db5458a95410e65118105c617c770
youssef <youssefelghamour@gmail.com>
2023-08-30 08:48:22 -0700

    correct typo in 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -28,7 +28,7 @@ int is_prime(int n, int m)
 {
 	if (m >= n && n > 1)
 		return (1);
-	else if (n % n == 0 || n <= 1)
+	else if (n % m == 0 || n <= 1)
 		return (0);
 	else
 		return (is_prime(n, m + 1));
