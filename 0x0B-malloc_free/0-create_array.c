4be14cf22759500ac28893c40a8e1d22677ff288
youssef <youssefelghamour@gmail.com>
2023-09-04 09:13:17 -0700

    define i outside for in 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,11 +9,12 @@
 char *create_array(unsigned int size, char c)
 {
 	char *s = malloc(size * sizeof(char));
+	unsigned int i = 0;
 
 	if (size == 0 || s == NULL)
 		return (NULL);
 
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		s[i] = c;
 
 	return (s);
