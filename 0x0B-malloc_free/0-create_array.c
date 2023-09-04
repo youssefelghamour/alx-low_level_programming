988b0e0a55e7541a0dc9a33aa2a01c167192e909
youssef <youssefelghamour@gmail.com>
2023-09-04 09:11:46 -0700

    change type of i in 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0 || s == NULL)
 		return (NULL);
 
-	for (int i = 0; i < size; i++)
+	for (unsigned int i = 0; i < size; i++)
 		s[i] = c;
 
 	return (s);
