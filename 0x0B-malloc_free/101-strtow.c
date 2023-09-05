75fef1da179d3f8c973ab5fd31c5c2206c7c09ca
youssef <youssefelghamour@gmail.com>
2023-09-05 08:51:16 -0700

    correct 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,9 +13,12 @@ char **strtow(char *str)
 
 	if (str[0] == '\0' || str == NULL)
 		return (NULL);
+
 	p = malloc((l + 1) * sizeof(char *));
+
 	if (p == NULL)
 		return (NULL);
+
 	for (i = 0; i < l; i++)
 	{
 		if (str[i] != ' ')
@@ -25,16 +28,21 @@ char **strtow(char *str)
 			while (str[i + word_length] != ' '
 					&& str[i + word_length] != '\0')
 				word_length++;
+
 			p[pos] = malloc((word_length + 1) * sizeof(char));
+
 			if (p[pos] == NULL)
 				return (NULL);
+
 			for (j = 0; j < word_length; j++)
 				p[pos][j] = str[i + j];
+
 			p[pos][j] = '\0';
 			pos++;
 			i += word_length;
 		}
 	}
 	p[pos] = '\0';
+
 	return (p);
 }
