544724614652aad25bd870e8d9b24ecb99d1ea9c
youssef <youssefelghamour@gmail.com>
2023-09-05 05:24:54 -0700

    correct 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	m = calloc(height, sizeof(int));
+	m = calloc(height, sizeof(int *));
 
 	if (m != NULL)
 	{
