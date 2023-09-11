0afa9851be993158498624faaa466a161b9865c1
youssef <youssefelghamour@gmail.com>
2023-09-11 14:58:00 -0700

    correct typo in 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	int name_len = 0, owner_len = 0, i, j;
 
-	dog_t *d = malloc(sizeof(struct dog));
+	dog_t *d = malloc(sizeof(dog_t));
 
 	if (d == NULL || name == NULL || owner == NULL)
 	{
