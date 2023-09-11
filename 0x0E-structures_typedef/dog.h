8c867978037da235cf0071e3931faa7e7e4172d7
youssef <youssefelghamour@gmail.com>
2023-09-11 13:48:31 -0700

    add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: dog's owner
+ *
+ * Description: structure named dog with elements: name, age & owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
