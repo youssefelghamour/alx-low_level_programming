f268b7479030a392e75737862bd8f41bb9de8d71
youssef <youssefelghamour@gmail.com>
2023-09-13 15:10:46 -0700

    add 100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,41 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int i, a;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	a = atoi(argv[1]);
+
+	if (a < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < a; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != a - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
