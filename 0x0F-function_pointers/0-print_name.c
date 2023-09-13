7b6c034819eeb38b2022e9d577867462f49ff5e5
youssef <youssefelghamour@gmail.com>
2023-09-13 11:16:34 -0700

    correct typo in 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
 
 /**
  * print_name - prints a name
@@ -9,6 +9,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if ( name && f)
-		f(name);
+	f(name);
 }
