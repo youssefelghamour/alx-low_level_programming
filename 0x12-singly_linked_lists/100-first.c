31b7899e96387e997646663ac5eb822ad0af7a99
youssef <youssefelghamour@gmail.com>
2023-09-21 12:05:16 -0700

    correct 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "lists.h"
 
 void _constructor(void) __attribute__ ((constructor));
 
@@ -8,6 +8,6 @@ void _constructor(void) __attribute__ ((constructor));
 
 void _constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
 }
