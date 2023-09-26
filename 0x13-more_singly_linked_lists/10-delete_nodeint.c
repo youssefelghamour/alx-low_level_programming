3a51ef1ff1879a6472088ef977cdd09027767910
youssef <youssefelghamour@gmail.com>
2023-09-26 12:45:50 -0700

    correct typo in 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,6 +11,7 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int i;
 	listint_t *prev, *current;
 
 	prev = *head;
@@ -29,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	else
 	{
-		for (int i = 0; i < index - 1; i++)
+		for (i = 0; i < index - 1; i++)
 			prev = prev->next;
 
 		current = prev->next;
