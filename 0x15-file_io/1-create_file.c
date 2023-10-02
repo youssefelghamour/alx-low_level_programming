aef01117d9bb06c9bc180a26e28f7384a2a41d61
youssef <youssefelghamour@gmail.com>
2023-10-02 03:35:19 -0700

    correct 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,12 +24,10 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	while (text_content[len])
-		len++;
+	for (len = 0; text_content[len]; len++)
+		;
 
-	wrt = write(STDOUT_FILENO, text_content, len);
-
-	if (wrt	== -1)
+	if ((write(STDOUT_FILENO, text_content, len)) == -1)
 		return (-1);
 
 	close(fd);
