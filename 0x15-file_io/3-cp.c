75583011c3edc87e3763e184e8539d7ff5e40cb4
youssef <youssefelghamour@gmail.com>
2023-10-02 09:25:11 -0700

    correct 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,6 +36,8 @@ int main(int ac, char **av)
 			exit(99);
 		}
 	}
+	if (len == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	if (close(file_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
