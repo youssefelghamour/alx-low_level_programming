0791f8cfcfe5818c785431913f6b5d48603856ee
youssef <youssefelghamour@gmail.com>
2023-10-02 09:14:51 -0700

    correct 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@
 
 int main(int ac, char **av)
 {
-	int res, fd1, fd2, len = 0;
+	int file_from, file_to, len = 0;
 	char buffer[1024];
 
 	if (ac != 3)
@@ -19,31 +19,31 @@ int main(int ac, char **av)
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd1 = open(av[1], O_RDONLY);
-	if (fd1 == -1)
+	file_from = open(av[1], O_RDONLY);
+	if (file_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fd2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((len = read(fd1, buffer, sizeof(buffer))) > 0)
+	file_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((len = read(file_from, buffer, sizeof(buffer))) > 0)
 	{
-		if ((write(fd2, buffer, len)) != len)
+		if ((write(file_to, buffer, len)) != len)
 		{
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
-			close(fd1);
-			close(fd2);
+			close(file_from);
+			close(file_to);
 			exit(99);
 		}
 	}
-	if (close(fd1) == -1)
+	if (close(file_from) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", fd1);
+		dprintf(2, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-	else if (close(fd2) == -1)
+	else if (close(file_to) == -1)
 	{
-		printf(2, "Error: Can't close fd %d\n", fd2);
+		dprintf(2, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
 
