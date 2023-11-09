d8007c6fc3af932abfe41fbe8a99027b93057772
youssef <youssefelghamour@gmail.com>
2023-11-09 03:44:38 -0800

    fix typo in 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
- * print_dlistint - counts the elements of a dlistint_t list
+ * dlistint_len - counts the elements of a dlistint_t list
  *
  * @h: pointer to the head of the list
  *
  * Return: number of nodes
  */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t n = 0;
 
