a9910103b0b2080f777a2b8e9b601d25e4f00f69
youssef <youssefelghamour@gmail.com>
2023-11-09 09:05:21 -0800

    correct 8-delete_dnodeint.c: code works but check 10 fails without detail

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,45 +7,88 @@
  * @index: index of the node that should be deleted
  *
  * Return: 1 if it succeeded, -1 if it failed
+ *
+ *int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+ *{
+ *    dlistint_t *before, *after, *middle;
+ *    unsigned int i = 0;
+ *
+ *    before = *head;
+ *
+ *    if (*head == NULL)
+ *        return (-1);
+ *
+ *    if (index == 0)
+ *    {
+ *        after = before->next;
+ *        if (after != NULL)
+ *            after->prev = NULL;
+ *        free(before);
+ *        *head = after;
+ *        return (1);
+ *    }
+ *
+ *    while (before)
+ *    {
+ *        after = before->next->next;
+ *        middle = before->next;
+ *
+ *        if (i == index - 1)
+ *        {
+ *            before->next = after;
+ *
+ *            if (after != NULL)
+ *                after->prev = before;
+ *
+ *            free(middle);
+ *            return (1);
+ *        }
+ *
+ *        i++;
+ *        before = before->next;
+ *    }
+ *
+ *    return (-1);
+ *}
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *before, *after, *middle;
-	unsigned int i = 0;
+	dlistint_t *h1;
+	dlistint_t *h2;
+	unsigned int i;
 
-	before = *head;
+	h1 = *head;
 
-	if (*head == NULL)
-		return (-1);
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
 
-	if (index == 0)
+	i = 0;
+
+	while (h1 != NULL)
 	{
-		after = before->next;
-		if (after != NULL)
-			after->prev = NULL;
-		free(before);
-		*head = after;
-		return (1);
-	}
-
-	while (before)
-	{
-		after = before->next->next;
-		middle = before->next;
-
-		if (i == index - 1)
+		if (i == index)
 		{
-			before->next = after;
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
 
-			if (after != NULL)
-				after->prev = before;
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
 
-			free(middle);
+			free(h1);
 			return (1);
 		}
-
+		h2 = h1;
+		h1 = h1->next;
 		i++;
-		before = before->next;
 	}
 
 	return (-1);
