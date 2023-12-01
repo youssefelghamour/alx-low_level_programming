76c7ef7bbd52acc2ff06180f92231e1d30ade6a3
youssef <youssefelghamour@gmail.com>
2023-12-01 07:53:43 -0800

    add 3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,64 @@
+#include "hash_tables.h"
+
+/**
+ * add_n_hash - adds a node at the beginning of the hash element
+ *
+ * @head: head of the hash element (linked list)
+ * @key: key
+ * @value: value
+ *
+ * Return: head of the linked list
+ */
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *tmp;
+
+	tmp = *head;
+
+	while (tmp != NULL)
+	{
+		if (strcmp(key, tmp->key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (*head);
+		}
+		tmp = tmp->next;
+	}
+
+	tmp = malloc(sizeof(hash_node_t));
+
+	if (tmp == NULL)
+		return (NULL);
+
+	tmp->key = strdup(key);
+	tmp->value = strdup(value);
+	tmp->next = *head;
+	*head = tmp;
+
+	return (*head);
+}
+
+/**
+ * hash_table_set - adds an element to the hash table
+ *
+ * @ht: pointer to the hash table
+ * @key: key
+ * @value: value
+ *
+ * Return: 1 if successes, 0 if fails
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+
+	if (key == NULL || *key == '\0')
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	if (add_node(&(ht->array[index]), key, value) == NULL)
+		return (0);
+
+	return (1);
+}
