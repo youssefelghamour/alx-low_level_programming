838903f961f0f52a0aa42a3c44b4771b5fdac80d
youssef <youssefelghamour@gmail.com>
2023-12-01 08:14:09 -0800

    correct 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -52,7 +52,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 
-	if (key == NULL || *key == '\0')
+	if (ht == NULL || ht->array == NULL || ht->size ==0 ||
+			key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
