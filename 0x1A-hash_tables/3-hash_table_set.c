10483a71222778e78adfc069d5789a0fc47cc8f1
youssef <youssefelghamour@gmail.com>
2023-12-01 08:15:05 -0800

    fix betty in 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -52,7 +52,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 
-	if (ht == NULL || ht->array == NULL || ht->size ==0 ||
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 			key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 
