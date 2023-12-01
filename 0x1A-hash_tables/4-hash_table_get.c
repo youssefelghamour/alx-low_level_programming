51ea3870ff22191c545b320b85bbc77cf46c2c5b
youssef <youssefelghamour@gmail.com>
2023-12-01 08:24:46 -0800

    fix typo in 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int k_index;
+	unsigned long int index;
 	hash_node_t *tmp;
 
 	if (ht == NULL || key == NULL || strlen(key) == '\0')
