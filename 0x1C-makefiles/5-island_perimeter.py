fb90ba0eb5529991ea431457752d2d2013aebcb2
youssef <youssefelghamour@gmail.com>
2024-01-24 06:55:06 -0800

    correct 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -20,13 +20,13 @@ def island_perimeter(grid):
         for column in range(len(grid[row])):
             if grid[row][column] == 1:
                 perimeter += 4
-                if grid[row-1][column] == 1:
+                if row > 0 and grid[row-1][column] == 1:
                     perimeter -= 1
-                if grid[row+1][column] == 1:
+                if row < len(grid) - 1 and grid[row+1][column] == 1:
                     perimeter -= 1
-                if grid[row][column-1] == 1:
+                if column > 0 and grid[row][column-1] == 1:
                     perimeter -= 1
-                if grid[row][column+1] == 1:
+                if column < len(grid[row]) - 1 and grid[row][column+1] == 1:
                     perimeter -= 1
 
     return perimeter
